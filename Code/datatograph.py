4516f6f48e899a369b9116d5e0e35a61c867bd3c
hamishwilson <hamish.wilson@student.manchester.ac.uk>
2018-03-23 03:08:08 +0000

    first set up data import programme

--- Code/datatograph.py
+++ Code/datatograph.py
@@ -1,4 +1,57 @@
 import csv
 
-with open('../Data/DATA.csv')
+data = []
+degreeOfRotation = 0
+flip = False
+lengthofdata = 0
+with open('../Data/DATA.csv','r' ) as csvfile: #Used to open csv file
+    reader = csv.reader(csvfile) #Used to read csv file
+    for row in reader:#Used to read each line
+        temp = [] #List to hold all elements in line
+        for element in row:
+            temp.append(element)
 
+        if temp[1] == '1':
+            temp.append('H')
+        elif temp[1] == '2':
+            temp.append('V')
+
+        if lengthofdata > 0:
+            if data[-1][-1] == 'H' and temp[4] == 'V':
+                degreeOfRotation = 180
+            else:
+                if temp[4] == 'H':
+                    if degreeOfRotation == 0:
+                        flip = False
+                    elif degreeOfRotation == 180:
+                        flip = True
+
+                    if not flip:  # If flip == false
+                        degreeOfRotation += 2
+                    elif flip:  # If flip == true
+                        degreeOfRotation -= 2
+
+                if temp[4] == 'V':
+                    if degreeOfRotation == 180:
+                        flip = False
+                    elif degreeOfRotation == 360:
+                        flip = True
+
+                    if not flip:  # If flip == false
+                        degreeOfRotation += 2
+                    elif flip:  # If flip == true
+                        degreeOfRotation -= 2
+
+        temp.append(degreeOfRotation)
+
+        # List to hold needed elements
+        temp2 = [temp[1], temp[3], temp[5], temp[4]]
+
+        # temp2.append
+
+        data.append(temp2)#List to hold final variables
+        lengthofdata += 1
+
+csvfile.close() #Close
+
+#print(data)
